098133cc0a91fd940e4f481b2ecd1067125d864e
Whoisbezo.fx <bezzo19@gmx.de>
2026-01-29 23:18:14 +0100

    layout.tsx aktualisieren

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <head />
-      <body className="min-h-screen bg-white antialiased">
+      <body className="min-h-screen bg-dark antialiased">
         <div className="flex min-h-screen flex-col">
           <main className="flex-1">{children}</main>
         </div>
