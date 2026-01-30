1ed4db00000a21d0b80995d5eb1765c76398a019
Whoisbezo.fx <bezzo19@gmx.de>
2026-01-30 19:28:21 +0100

    page.tsx aktualisieren

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,17 @@ import { Button } from "@/components/ui/button"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 export default function IndexPage() {
-   return 
-    <Avatar>
-          <AvatarImage
-            src="https://github.com/shadcn.png"
-            alt="@shadcn"
-            className="grayscale"
-            />
-          <AvatarFallback>CN</AvatarFallback>
-    </Avatar>
-    <Button variant="outline">Outline</Button>
+  return (
+    <div>
+      <Avatar>
+        <AvatarImage
+          src="https://github.com/shadcn.png"
+          alt="@shadcn"
+          className="grayscale"
+        />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+      <Button variant="outline">Outline</Button>
+    </div>
+  )
 }
