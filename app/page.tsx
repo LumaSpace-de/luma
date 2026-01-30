5a099758365eeff29bc792fbcd927715894a17c2
Whoisbezo.fx <bezzo19@gmx.de>
2026-01-30 19:26:38 +0100

    page.tsx aktualisieren

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,17 @@ import Link from "next/link"
 
 import { siteConfig } from "@/config/site"
 import { Button } from "@/components/ui/button"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 export default function IndexPage() {
-   return <Button variant="outline">Outline</Button>
+   return 
+    <Avatar>
+          <AvatarImage
+            src="https://github.com/shadcn.png"
+            alt="@shadcn"
+            className="grayscale"
+            />
+          <AvatarFallback>CN</AvatarFallback>
+    </Avatar>
+    <Button variant="outline">Outline</Button>
 }
