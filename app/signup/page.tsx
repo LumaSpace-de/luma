bdcbb12d2e528f4ad179ee9f5b19d57dae5317cb
Whoisbezo.fx <bezzo19@gmx.de>
2026-02-01 02:22:22 +0100

    page.tsx aktualisieren

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,5 +1,6 @@
 import { GalleryVerticalEnd } from "lucide-react"
 
+import { DotPattern } from "@/components/dot-pattern"
 import { SignupForm } from "@/components/signup-form"
 
 export default function SignupPage() {
