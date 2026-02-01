323ed6f354ca4b6b7bfee753e79f1303cb232d20
Whoisbezo.fx <bezzo19@gmx.de>
2026-02-01 14:38:02 +0100

    page.tsx aktualisieren

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,5 +1,4 @@
 import { GalleryVerticalEnd } from "lucide-react"
-
 import { DotPattern } from "@/components/dot-pattern"
 import { SignupForm } from "@/components/signup-form"
 
@@ -15,17 +14,17 @@ export default function SignupPage() {
       glowIntensity={0.8}
       waveSpeed={0.3}
     >
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            <GalleryVerticalEnd className="size-4" />
-          </div>
-          LumaSpace
-        </a>
-        <SignupForm />
+      <div className="relative z-10 flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+        <div className="flex w-full max-w-sm flex-col gap-6">
+          <a href="#" className="flex items-center gap-2 self-center font-medium">
+            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+              <GalleryVerticalEnd className="size-4" />
+            </div>
+            LumaSpace
+          </a>
+          <SignupForm />
+        </div>
       </div>
-    </div>
     </DotPattern>
   )
 }
