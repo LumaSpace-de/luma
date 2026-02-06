c450273e147436f52f4896a59ccdf8c1cf8890af
Whoisbezo.fx <bezzo19@gmx.de>
2026-02-07 00:46:49 +0100

    page.tsx aktualisieren

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,4 +1,3 @@
-import { GalleryVerticalEnd } from "lucide-react"
 import { DotPattern } from "@/components/dot-pattern"
 import { SignupForm } from "@/components/signup-form"
 
@@ -18,9 +17,7 @@ export default function SignupPage() {
         <div className="flex w-full max-w-sm flex-col gap-6">
           <a href="#" className="flex items-center gap-2 self-center font-medium">
             <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-              <GalleryVerticalEnd className="size-4" />
             </div>
-            LumaSpace
           </a>
           <SignupForm />
         </div>
