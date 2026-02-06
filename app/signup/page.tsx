da3046d7af49dd3b3b08e1450d74cc5530caeffe
Whoisbezo.fx <bezzo19@gmx.de>
2026-02-07 00:49:02 +0100

    page.tsx aktualisieren

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -15,10 +15,6 @@ export default function SignupPage() {
     >
       <div className="relative z-10 flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
         <div className="flex w-full max-w-sm flex-col gap-6">
-          <a href="#" className="flex items-center gap-2 self-center font-medium">
-            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            </div>
-          </a>
           <SignupForm />
         </div>
       </div>
