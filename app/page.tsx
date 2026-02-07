bd657fff0c178b41d13d4e6f490dbf85c4403216
Whoisbezo.fx <bezzo19@gmx.de>
2026-02-07 17:29:45 +0100

    page.tsx aktualisieren

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,6 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 export default function IndexPage() {
   return (
     <div className="absolute right-4 top-4 z-50 flex gap-2">
-        <Button variant="outline" size="sm">
-          Outline
-        </Button>
         <Link href="/login">
           <Button size="sm">
             Login
