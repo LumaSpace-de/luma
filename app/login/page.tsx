7773bbbe726941e02cb7247f6bdc57727bb21929
Whoisbezo.fx <bezzo19@gmx.de>
2026-02-07 01:43:33 +0100

    page.tsx aktualisieren

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,3 @@
-import { GalleryVerticalEnd } from "lucide-react"
 import { DotPattern } from "@/components/dot-pattern"
 import { LoginForm } from "@/components/login-form"
 
@@ -14,13 +13,8 @@ export default function LoginPage() {
       glowIntensity={0.8}
       waveSpeed={0.3}
     >
-      <div className="relative z-10 flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="relative z-10 flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
         <div className="flex w-full max-w-sm flex-col gap-6">
-          <a href="#" className="flex items-center gap-2 self-center font-medium text-white">
-            <div className="bg-primary text-primary-foreground flex size-8 items-center justify-center rounded-lg">
-              <GalleryVerticalEnd className="size-5" />
-            </div>
-          </a>
           <LoginForm />
         </div>
       </div>
