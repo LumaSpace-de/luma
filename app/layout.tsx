69ac69cd4f6244f5cfb32c1d46c0a5a7544cb82a
Whoisbezo.fx <bezzo19@gmx.de>
2026-02-14 23:20:43 +0100

    layout.tsx aktualisieren

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,21 +19,11 @@ export default function RootLayout({
         )}
       >
         <SidebarProvider>
-          {/* SHADCN ROOT */}
-          <div className="flex min-h-screen w-full">
-            <AppSidebar />
-
-            <div className="flex flex-1 flex-col">
-              <header className="flex h-14 items-center gap-2 border-b px-4">
-                <SidebarTrigger />
-              </header>
-
-              <main className="flex-1 overflow-auto p-4">
-                {children}
-              </main>
-            </div>
-          </div>
-        </SidebarProvider>
+      <div className="flex">
+        <AppSidebar />
+        <main className="flex-1">{children}</main>
+      </div>
+    </SidebarProvider>
       </body>
     </html>
   )
