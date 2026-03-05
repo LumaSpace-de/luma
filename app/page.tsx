30f00c770958eff783a2f8bc042d385893e0bb48
Whoisbezo.fx <bezzo19@gmx.de>
2026-03-05 21:29:31 +0100

    page.tsx aktualisieren

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
 import Link from "next/link"
+import Image from "next/Image"
 
-import { siteConfig } from "@/config/site"
+// Components //
+
 import { Button } from "@/components/ui/button"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 export default function IndexPage() {
   return (
+    <div className="flex justify-between items-center p-4">
+
+      {/* Logo */}
+      <div className="flex items-center gap-2">
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          width={32}
+          height={32}
+        />
+        <span className="font-bold">LumaSpace</span>
+      </div>
     <div className="absolute right-4 top-4 z-50 flex gap-2">
         <Link href="/login">
           <Button size="sm">
