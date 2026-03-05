57418058cedf9c1fb6177db75b61c009de497895
Whoisbezo.fx <bezzo19@gmx.de>
2026-03-05 21:32:47 +0100

    page.tsx aktualisieren

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ import { Button } from "@/components/ui/button"
 export default function IndexPage() {
   return (
     <div className="flex justify-between items-center p-4">
-
       {/* Logo */}
       <div className="flex items-center gap-2">
         <Image
@@ -19,11 +18,9 @@ export default function IndexPage() {
         />
         <span className="font-bold">LumaSpace</span>
       </div>
-    <div className="absolute right-4 top-4 z-50 flex gap-2">
-        <Link href="/login">
-          <Button size="sm">
-            Login
-          </Button>
+      <div className="absolute right-4 top-4 z-50 flex gap-2">
+          <Link href="/login">
+            <Button size="sm">Login</Button>
         </Link>
       </div>
   )
